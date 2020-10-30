aea346dfddd75fbe1ed7e902fd1d599f968a950e
Qubits-ops <72653472+Qubits-ops@users.noreply.github.com>
2020-10-30 02:43:19 +0100

    Update math-physique.h

--- math-physique.h
+++ math-physique.h
@@ -22,11 +22,17 @@ int puissance(int m,int pui){
     printf("%d\n",a);
 
 }
-float conversion(int fahr){
+float conversion_fahr_celsius(int fahr){
     float celsius;
     celsius = (5.0/9.0) * (fahr - 32.0);
     printf("%.1f",celsius);
 }
+float conversion_celsius_fahr(float celsius){
+    float fahr;
+    fahr = celsius * 9.0/5.0 + 32.0;
+    printf("%.1f\n",fahr);
+
+};
 
 float table_fahr_celsius(){
     float fahr,celsius;
@@ -71,6 +77,50 @@ int masse_volumique(int m, int ro){
     int v;
     v = m/ro;
     printf("le volume vaux %d\n",v);
+}
+
+int additions(int nb1,int nb2){
+    int resultat;
+    resultat = nb1 + nb2;
+    printf("%d\n",resultat);
+}
+int soustractions(int nb1,int nb2){
+    int resultat;
+    resultat = nb1 - nb2;
+    printf("%d\n",resultat);
+}
+int mulitplication(int nb1,int nb2){
+    int resultat;
+    resultat = nb1 * nb2;
+    printf("%d\n",resultat);
+}
+int division(int nb1,int nb2){
+    if(nb2 == 0){
+        printf("la division par 0 est impossible\n");
+    }
+    int resultat;
+    resultat = nb1/nb2;
+    printf("%d\n",resultat);
+
+
+}
+int fibo(int n){
+    /*En developpement*/
+    int pre,cou,i,sui;
+    if(n < 2){
+        return n;
+    }
+    else{
+        pre = 1;
+        cou = 1;
+        for(i=2;i< n;n-2){
+            sui = p + c;
+            p = c;
+            c = s;
+        }
+        return c;
+    }
+
 }
 
 #endif // MATH-PHYSIQUE_H_INCLUDED
