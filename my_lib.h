20eb4451d16e889f1358f5119387a73dad586a0d
Qubits-ops <72653472+Qubits-ops@users.noreply.github.com>
2020-11-15 23:13:19 +0100

    Update my_lib.h

--- my_lib.h
+++ my_lib.h
@@ -1,9 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
-void my_putchar(char c){
-	write(1,&c,1);
-	
+void my_putchar(c)
+{
+    write(1,&c,1);
+}
+
+int my_printf(const char *str){
+    int i;
+    i = 0;
+    while(str[i] != '\0'){
+        my_putchar(str[i]);
+        i++;
+    }
+    my_putchar('\0');
+    return 0;
+}
+
+int my_strlen(const char *str){
+    int i;
+    i = 0;
+    while (str[i] != '\0'){
+        i++;
+    }
+    return i;
 }
 
 
+int main(){
+    my_printf("salut\n");
+    my_putchar('\n');
+    my_putchar('a');
+    return 0;
+}
